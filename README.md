755efc3ab2a37fcbb56f9b8dc6c284e4269d83dd
ramavath <ramavathdhakya2025@gmail.com>
2026-03-12 15:07:10 +0530

    requirements.txt is changed

--- README.md
+++ README.md
@@ -5,5 +5,10 @@ Steps to Deploy the website using the Gunicorn
 3. Add .env file at top level
 4. Add .gitignore file at top level
 5. Change the debugger to false
-6. Add and install gunicorn packege in your project as well as in requirements 
-7. 
+6. Add and install gunicorn packege in your project as well as in requirements.
+#################### Deploy ################
+7. Login in render server with github
+8. Create the new web service
+9. add environment variables
+10. Change the start command to $gunicorn main:app
+11. 
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
-from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Integer, String, DateTime, Boolean
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -8,8 +7,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQL_ALCHEMY_URI")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQL_ALCHEMY_URI", "sqlite:///tracker.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 class Base(DeclarativeBase):
@@ -39,7 +38,6 @@ DAYS = [d for d in range(1, 22)]
 
 @app.route('/')
 def home():
-    # Load saved progress
     progress = {}
     for entry in Tracker.query.all():
         progress[(entry.day, entry.task)] = entry.completed
@@ -47,16 +45,13 @@ def home():
 
 @app.route('/save', methods=['POST'])
 def save():
-    # Clear old entries
     Tracker.query.delete()
-
     for day in DAYS:
         for task in TASKS:
             field_name = f"day{day}_{task}"
             completed = field_name in request.form
             tracker_entry = Tracker(day=day, task=task, completed=completed)
             db.session.add(tracker_entry)
-
     db.session.commit()
     return redirect(url_for('home'))
 
